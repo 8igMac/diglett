9446de5e89b5aa804c9cf0a21aceff5013592c95
8igmac <8igmac@pm.me>
2022-05-28 10:55:11 +0800

    [TEST] Speaker embedding similiarity.

---
+++ test_same_person_emb.py
@@ -0,0 +1,61 @@
+import torchaudio
+from speechbrain.pretrained import EncoderClassifier, SpeakerRecognition
+
+def main():
+    # tmpdir = getfixture("tmpdir")
+    tmpdir = './model/speaker_recog'
+    # classifier = EncoderClassifier.from_hparams(
+    #     source="speechbrain/spkrec-ecapa-voxceleb",
+    #     savedir=tmpdir,
+    # )
+
+    verification = SpeakerRecognition.from_hparams(
+        source="speechbrain/spkrec-ecapa-voxceleb",
+        savedir=tmpdir,
+    )
+
+    print("Same speaker, with score")
+    # Male
+    signal1, fs1 = torchaudio.load("data/speaker_m/audio-oz-julia-000.wav")
+    signal3, fs3 = torchaudio.load("data/speaker_m/audio-oz-julia-002.wav")
+    signal5, fs5 = torchaudio.load("data/speaker_m/audio-oz-julia-010.wav")
+    score, pred = verification.verify_batch(signal1, signal5)
+    print(score)
+    score, pred = verification.verify_batch(signal3, signal5)
+    print(score)
+    score, pred = verification.verify_batch(signal3, signal1)
+    print(score)
+
+    # Female
+    signal2, fs2 = torchaudio.load("data/speaker_f/audio-oz-julia-001.wav")
+    signal4, fs4 = torchaudio.load("data/speaker_f/audio-oz-julia-007.wav")
+    signal6, fs6 = torchaudio.load("data/speaker_f/audio-oz-julia-009.wav")
+    score, pred = verification.verify_batch(signal2, signal4)
+    print(score)
+    score, pred = verification.verify_batch(signal6, signal4)
+    print(score)
+    score, pred = verification.verify_batch(signal6, signal2)
+    print(score)
+
+    print("Different speaker, with score")
+    score, pred = verification.verify_batch(signal1, signal2)
+    print(score)
+    score, pred = verification.verify_batch(signal1, signal4)
+    print(score)
+    score, pred = verification.verify_batch(signal1, signal6)
+    print(score)
+    score, pred = verification.verify_batch(signal3, signal2)
+    print(score)
+    score, pred = verification.verify_batch(signal3, signal4)
+    print(score)
+    score, pred = verification.verify_batch(signal3, signal6)
+    print(score)
+    score, pred = verification.verify_batch(signal5, signal2)
+    print(score)
+    score, pred = verification.verify_batch(signal5, signal4)
+    print(score)
+    score, pred = verification.verify_batch(signal5, signal6)
+    print(score)
+
+if __name__ == "__main__":
+    main()
