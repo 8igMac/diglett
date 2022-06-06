fdaa6699248d9e83bbc633c7b4f2265347c93655
8igmac <8igmac@pm.me>
2022-06-06 17:14:19 +0800

    [TEST] Config, stream client.

--- server.py
+++ server.py
@@ -1,38 +1,95 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-import wave
-import pyaudio
 import asyncio
 import json
-import time
+import base64
+from speechbrain.pretrained import VAD
 
-# TODO: How to encode these data? 
-chunk = 1024  # Record in chunks of 1024 samples
-sample_format = pyaudio.paInt16  # 16 bits per sample
-channels = 2
-fs = 44100  # Record at 44100 samples per second
-seconds = 3
-filename = "server_output.wav"
+from utils import bytes_preproc
+
 app = FastAPI()
+tmpdir = './model/vad'
+vad = VAD.from_hparams(
+    source="speechbrain/vad-crdnn-libriparty",
+    savedir=tmpdir,
+)
 
-# with wave.open(filename, "wb") as wf:
-#     wf.setnchannels(channels)
-#     wf.setsampwidth(pyaudio.get_sample_size(sample_format))
-#     wf.setframerate(fs)
-#     wf.writeframes(b''.join(frames))
-    
+@app.websocket("/ws/config")
+async def config(websocket: WebSocket):
+    """ Configuration
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
+    Extract speaker embedding from a audio segment.
+
+    Input: A speech segment.
+    Ouput: Speaker embedding, (ASR?).
+    """
     await websocket.accept()
 
     try:
-        async for data in websocket.iter_bytes():
-            eos_b = bytes("EOS", "utf-8")
-            length = len(eos_b)
-            if data[-length:] == eos_b:
+        message = await websocket.receive_json()
+        if "audio_data" in message:
+            print("server config: got audio data")
+
+            # Decode audio.
+            encoded = message["audio_data"]
+            audio_data = base64.b64decode(encoded)
+
+            # Send message.
+            message = {
+                "speaker_name": "小芬姊",
+                "speaker_embedding": [0.5, 0.1],
+            }
+
+            # Send audio label.
+            await websocket.send_json(message)
+        else:
+            print("Wrong format: no 'audio_data'")
+    except WebSocketDisconnect:
+        print("Client disconnected.")
+
+@app.websocket("/ws/stream")
+async def websocket_endpoint(websocket: WebSocket):
+    """Streaming
+
+    Stream audio input and return speaker labels.
+
+    Input: Continuous audio stream.
+        {
+            "audio_data": [base64 encoded bytes],
+            "terminate_session": bool,
+        }
+    Output: Continuous label stream.
+        {
+            "speaker": str,
+            "db": float,
+        }
+    """
+    await websocket.accept()
+
+    try:
+        async for message in websocket.iter_json():
+            if "terminate_session" in message and message["terminate_session"] == True: 
+                print("terminate session")
                 break
-            else:
-                # data = bytes("get message", "utf-8")
-                await websocket.send_bytes(data)
+
+            # Decode audio.
+            encoded = message["audio_data"]
+            audio_data = base64.b64decode(encoded)
+
+            # Decode speaker embedding.
+            spks = message["speaker_embedding"]
+            assert len(spks) == 2
+            spk1, spk2 = spks
+            print(f"Speaker1: {spk1}")
+            print(f"Speaker2: {spk2}")
+
+            # TODO: VAD
+            # TODO: Speaker verification.
+            message = {
+                "speaker": "alice",
+                "db": 56,
+            }
+
+            # Send audio label.
+            await websocket.send_json(message)
     except WebSocketDisconnect:
         print("Client disconnected.")
