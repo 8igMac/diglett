069f0f3819ebb0a645822a7212bcb2efc90e86aa
8igmac <8igmac@pm.me>
2022-06-08 16:15:50 +0800

    [FEAT] Add wav file read/write to utils.

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import torch
 import numpy as np
+import wave
+import pyaudio
 
 import config
 
@@ -28,3 +30,26 @@ def bytes_preproc(raw_bytes: bytes):
     data = data / 32768
 
     return data
+
+def read_wav_file(filename:str):
+    """
+    Args:
+        filename: path to audio file.
+    Return:
+        Raw audio in bytes.
+    """
+    frames = list()
+    with wave.open(filename, "rb") as wf:
+        data = wf.readframes(config.chunk)
+        while len(data) > 0:
+            frames.append(data)
+            data = wf.readframes(config.chunk)
+    return b"".join(frames)
+
+def write_wav_file(filename: str, raw_audio: bytes):
+    with wave.open(filename, "wb") as wf:
+        wf.setnchannels(config.channels)
+        wf.setsampwidth(pyaudio.get_sample_size(config.sample_format))
+        wf.setframerate(config.fs)
+        wf.writeframes(raw_audio)
+
